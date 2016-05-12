b429d87d8bf4a5952c692044f4658a14f1168f66
harendranath <harendranath.v@gmail.com>
2016-05-12 11:04:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+(function(angular) 
+{
+
+  'use strict';
+angular.module('docsBindExample', [])
+ 
+ .controller('Controller', ['$scope', function($scope) 
+{
+ 
+   $scope.name = 'hari (April 20, 2016 – October 4, 2016)';
+  }]);
+})(window.angular);
