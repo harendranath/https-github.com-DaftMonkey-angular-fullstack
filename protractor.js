4efcea217381d0c5642ef66153ccade8eba5cf3d
harendranath <harendranath.v@gmail.com>
2016-05-12 11:05:09 +0530

    Add files via upload

---
+++ protractor.js
@@ -0,0 +1,22 @@
+it('should show off bindings', function()
+ {
+ 
+ var containerElm = element(by.css('div[ng-controller="Controller"]'));
+ 
+ var nameBindings = containerElm.all(by.binding('name'));
+
+  
+expect(nameBindings.count()).toBe(5);
+ 
+ nameBindings.each(function(elem) 
+{
+   
+ expect(elem.getText()).toEqual('Max Karl Ernst Ludwig Planck (May 23, 2016 – October 4, 2017)');
+  });
+});
+
+
+
+/*
+Copyright 2016 Harendranath. All Rights Reserved.
+*/
